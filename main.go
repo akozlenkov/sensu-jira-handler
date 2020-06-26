627ca774cd0f353cdd6791a62d8078fd84a4309c
Andrey Kozlenkov <a.kozlenkov@hh.ru>
2020-06-26 19:55:22 +0300

    added tpls for summary and description

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
-	"gopkg.in/andygrunwald/go-jira.v1"
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"gopkg.in/andygrunwald/go-jira.v1"
 	"os"
+	"text/template"
 )
 
 type HandlerConfig struct {
 	sensu.PluginConfig
-	url   		string
-	user 		string
-	password	string
-	project 	string
-	issueType   string
-	summary		string
-	description string
+	jiraUrl             string
+	jiraUser            string
+	jiraPassword        string
+	jiraProject         string
+	jiraIssueType       string
+	jiraSummaryTmpl     string
+	jiraDescriptionTmpl string
 }
 
 var (
@@ -30,137 +33,176 @@ var (
 
 	configOptions = []*sensu.PluginConfigOption{
 		{
-			Path:      "url",
-			Env:       "JIRA_URL",
-			Argument:  "url",
-			Usage:     "The jira url",
-			Value:     &config.url,
+			Path:     "jira-url",
+			Env:      "JIRA_URL",
+			Argument: "jira-url",
+			Usage:    "The jira URL",
+			Value:    &config.jiraUrl,
 		},
 		{
-			Path:      "user",
-			Env:       "JIRA_USER",
-			Argument:  "user",
-			Usage:     "The jira user",
-			Value:     &config.user,
+			Path:     "jira-user",
+			Env:      "JIRA_USER",
+			Argument: "jira-user",
+			Usage:    "The jira user",
+			Value:    &config.jiraUser,
 		},
 		{
-			Path:      "password",
-			Env:       "JIRA_PASSWORD",
-			Argument:  "password",
-			Usage:     "The jira password",
-			Value:     &config.user,
+			Path:     "jira-password",
+			Env:      "JIRA_PASSWORD",
+			Argument: "jira-password",
+			Usage:    "The jira password",
+			Value:    &config.jiraPassword,
 		},
 		{
-			Path:      "project",
-			Env:       "JIRA_PROJECT",
-			Argument:  "project",
-			Usage:     "The jira project ID",
-			Value:     &config.project,
+			Path:     "jira-project",
+			Env:      "JIRA_PROJECT",
+			Argument: "jira-project",
+			Usage:    "The jira project key",
+			Value:    &config.jiraProject,
 		},
 		{
-			Path:      "issue_type",
-			Env:       "JIRA_ISSUE_TYPE",
-			Argument:  "issue_type",
-			Usage:     "The jira issue type ID",
-			Value:     &config.issueType,
+			Path:     "jira-issue-type",
+			Env:      "JIRA_ISSUE_TYPE",
+			Argument: "jira-issue-type",
+			Usage:    "The jira issue type",
+			Value:    &config.jiraIssueType,
 		},
 		{
-			Path:      "summary",
-			Argument:  "summary",
-			Usage:     "The jira issue summary",
-			Value:     &config.summary,
+			Path:     "jira-summary",
+			Env:      "JIRA_SUMMARY",
+			Argument: "jira-summary",
+			Usage:    "The template to use to populate the issue summary",
+			Default:  "Check {{ .Name }} fired with status {{ .Status }}",
+			Value:    &config.jiraSummaryTmpl,
 		},
 		{
-			Path:      "description",
-			Argument:  "description",
-			Usage:     "The jira issue description",
-			Value:     &config.description,
+			Path:     "jira-description",
+			Env:      "JIRA_DESCRIPTION",
+			Argument: "jira-description",
+			Usage:    "The template to use to populate the issue description",
+			Default:  "{{ .Output }}",
+			Value:    &config.jiraDescriptionTmpl,
 		},
 	}
 )
 
-func main()  {
+func main() {
 	goHandler := sensu.NewGoHandler(&config.PluginConfig, configOptions, checkArgs, sendMessage)
 	goHandler.Execute()
 }
 
 func checkArgs(_ *corev2.Event) error {
-	if url := os.Getenv("JIRA_URL"); url != "" {
-		config.url = url
+	if jiraUrl := os.Getenv("JIRA_URL"); jiraUrl != "" {
+		config.jiraUrl = jiraUrl
 	}
 
-	if user := os.Getenv("JIRA_USER"); user != "" {
-		config.user = user
+	if jiraUser := os.Getenv("JIRA_USER"); jiraUser != "" {
+		config.jiraUser = jiraUser
 	}
 
-	if password := os.Getenv("JIRA_PASSWORD"); password != "" {
-		config.password = password
+	if jiraPassword := os.Getenv("JIRA_PASSWORD"); jiraPassword != "" {
+		config.jiraPassword = jiraPassword
 	}
 
-	if project := os.Getenv("JIRA_PROJECT"); project != "" {
-		config.project = project
+	if jiraProject := os.Getenv("JIRA_PROJECT"); jiraProject != "" {
+		config.jiraProject = jiraProject
 	}
 
-	if issueType := os.Getenv("JIRA_ISSUE_TYPE"); issueType != "" {
-		config.issueType = issueType
+	if jiraIssueType := os.Getenv("JIRA_ISSUE_TYPE"); jiraIssueType != "" {
+		config.jiraIssueType = jiraIssueType
 	}
 
-	if len(config.url) == 0 {
-		return fmt.Errorf("--url or JIRA_URL environment variable is required")
+	if jiraSummaryTmpl := os.Getenv("JIRA_SUMMARY"); jiraSummaryTmpl != "" {
+		config.jiraSummaryTmpl = jiraSummaryTmpl
 	}
 
-	if len(config.user) == 0 {
-		return fmt.Errorf("--user or JIRA_USER environment variable is required")
+	if jiraDescriptionTmpl := os.Getenv("JIRA_DESCRIPTION"); jiraDescriptionTmpl != "" {
+		config.jiraDescriptionTmpl = jiraDescriptionTmpl
 	}
 
-	if len(config.password) == 0 {
-		return fmt.Errorf("--password or JIRA_PASSWORD environment variable is required")
+	if len(config.jiraUrl) == 0 {
+		return fmt.Errorf("--jira-url or JIRA_URL environment variable is required")
 	}
 
-	if len(config.project) == 0 {
-		return fmt.Errorf("--project or JIRA_PROJECT environment variable is required")
+	if len(config.jiraUser) == 0 {
+		return fmt.Errorf("--jira-user or JIRA_USER environment variable is required")
 	}
 
-	if len(config.issueType) == 0 {
-		return fmt.Errorf("--issue_type or JIRA_ISSUE_TYPE environment variable is required")
+	if len(config.jiraPassword) == 0 {
+		return fmt.Errorf("--jira-password or JIRA_PASSWORD environment variable is required")
 	}
 
-	if len(config.summary) == 0 {
-		return fmt.Errorf("--summary is required")
+	if len(config.jiraProject) == 0 {
+		return fmt.Errorf("--jira-project or JIRA_PROJECT environment variable is required")
 	}
 
-	if len(config.description) == 0 {
-		return fmt.Errorf("--description is required")
+	if len(config.jiraIssueType) == 0 {
+		return fmt.Errorf("--jira-issue_type or JIRA_ISSUE_TYPE environment variable is required")
 	}
-
 	return nil
 }
 
 func sendMessage(event *corev2.Event) error {
 	auth := jira.BasicAuthTransport{
-		Username: config.user,
-		Password: config.password,
+		Username: config.jiraUser,
+		Password: config.jiraPassword,
 	}
 
-	client, err := jira.NewClient(auth.Client(), config.url)
+	client, err := jira.NewClient(auth.Client(), config.jiraUrl)
 	if err != nil {
+		exitOnErr(err)
+	}
+
+	var project bytes.Buffer
+	projectTmpl, err := template.New("project").Parse(config.jiraProject)
+	if err != nil {
+		exitOnErr(err)
+	}
+	if err = projectTmpl.Execute(&project, event.Check); err != nil {
+		exitOnErr(err)
+	}
+
+	var issueType bytes.Buffer
+	issueTypeTmpl, err := template.New("issueType").Parse(config.jiraIssueType)
+	if err != nil {
+		exitOnErr(err)
+	}
+	if err = issueTypeTmpl.Execute(&issueType, event.Check); err != nil {
+		exitOnErr(err)
+	}
+
+	var summary bytes.Buffer
+	summaryTmpl, err := template.New("summary").Parse(config.jiraSummaryTmpl)
+	if err != nil {
+		exitOnErr(err)
+	}
+	if err = summaryTmpl.Execute(&summary, event.Check); err != nil {
+		exitOnErr(err)
+	}
+
+	var description bytes.Buffer
+	descriptionTmpl, err := template.New("description").Parse(config.jiraDescriptionTmpl)
+	if err != nil {
+		exitOnErr(err)
+	}
+	if err = descriptionTmpl.Execute(&description, event.Check); err != nil {
 		exitOnErr(err)
 	}
 
 	_, _, err = client.Issue.Create(&jira.Issue{
 		Fields: &jira.IssueFields{
 			Type: jira.IssueType{
-				ID: config.issueType,
+				Name: issueType.String(),
 			},
 			Project: jira.Project{
-				ID: config.project,
+				Key: project.String(),
 			},
-			Summary: config.summary,
-			Description: config.description,
+			Summary:     summary.String(),
+			Description: description.String(),
 		},
 	})
 	if err != nil {
-		exitOnErr(err)
+		exitOnErr(errors.New("unable create jira ticket"))
 	}
 	return nil
 }
